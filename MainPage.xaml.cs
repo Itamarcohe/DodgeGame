f9f1f8bb170059af4c39e7ee903e92973951a10d
Itamar Cohen <Itamar330@gmail.com>
2023-08-24 18:44:36 +0300

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using DodgeGame.Classes;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+
+namespace DodgeGame
+{
+    public sealed partial class MainPage : Page
+    {
+        
+        GameManager gameManager;
+
+        MusicManager music = new MusicManager();
+
+        public MainPage()
+        {
+
+            this.InitializeComponent();
+
+            gameManager = new GameManager(MyCanvas, music, timerTextBlock, SaveGameBtn, LoadGameButton);
+
+            gameManager.NewGame();
+
+        }
+        //
+
+
+        private void PauseResumeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (gameManager.IsGameRunning)
+            {
+
+                music.PauseBgMusic();
+
+                gameManager.IsGameRunning = false;
+                gameManager.player._isAlive = false;
+                PauseResumeButton.Content = "Resume";
+
+                gameManager.gameTimer.Stop();
+                gameManager.timer.Stop();
+
+            }
+            else
+            {
+                music.ResumePlayingBGMusic(); //Unpause the music
+
+                gameManager.IsGameRunning = true;
+                gameManager.player._isAlive = true;
+                PauseResumeButton.Content = "Pause";
+
+                gameManager.gameTimer.Start();
+                gameManager.timer.Start();
+
+            }
+        }
+
+
+        private void RestartGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            gameManager.NewGame();
+
+        }
+
+
+        //
+    }
+
+}
